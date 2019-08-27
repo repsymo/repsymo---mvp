81bc106755ab2b09663b1b4d15e06730ab72b20e
Tobias Briones <tobiasbrionesdev@gmail.com>
2019-08-26 21:25:43 -0600

    WM added to the model: fire cost, quit number

--- src/app/solver/WMSolver.ts
+++ src/app/solver/WMSolver.ts
@@ -8,6 +8,8 @@ export interface Model {
     newEmployeeFixedCost: number,
     newEmployeePerWeekCost: number,
     initialNumberOfEmployees: number,
+    fireEmployeeCost: number,
+    quitEmployees: number,
     workforceWeeks: WorkforcePerWeek[]
 }
 
@@ -50,6 +52,7 @@ export class WMSolver {
     private stages: Stage[];
     private max: number;
     private path: number[];
+    private cost: number[];
     private interpretation: string[];
     
     constructor() {
@@ -61,6 +64,7 @@ export class WMSolver {
         this.stages = null;
         this.max = -1;
         this.path = [];
+        this.cost = [];
         this.interpretation = [];
     }
     
@@ -70,13 +74,17 @@ export class WMSolver {
         const C1 = this.model.manpowerExcessCost;
         const C2 = this.model.newEmployeePerWeekCost;
         const C3 = this.model.newEmployeeFixedCost;
+        const F = this.model.fireEmployeeCost;
         const isHiringNewEmployees = (_x: number, demand: number): boolean => {
             // New employees are hired for this week iff the number of employees from the
             // previous week is less than those of this week
             return _x < demand;
         }
+        const isFiringExistingEmployees = (_x: number, demand: number): boolean => {
+            return demand < _x;
+        }
         const getOptionValueAt = (demand: number): number => {
-            // Cero by definition for the last week
+            // Cero by definition for the last stage
             if(week >= this.stages.length - 1) return 0;
             const nextStage: Stage = this.stages[week + 1];
             const lookupRow: StageRow = nextStage.rows.find(row => row.previousX == demand);
@@ -86,9 +94,11 @@ export class WMSolver {
             // Calculate the cost for the cost options array
             // If this stage is hiring new employees then sum up the cost of hiring
             const hasNewEmployees = isHiringNewEmployees(_x, demand);
+            const hasToFireEmployees = isFiringExistingEmployees(_x, demand);
             const newStaffCost = (hasNewEmployees) ? C3 + C2 * (demand - _x) : 0;
+            const firingCost = (hasToFireEmployees) ? F * (_x - demand) : 0;
             const f_ = getOptionValueAt(demand);
-            return C1 * (demand - minimumDemand) + newStaffCost + f_;
+            return C1 * (demand - minimumDemand) + newStaffCost + firingCost + f_;
         }
         const getMinimum = (options: CostOption[]): CostOption => {
             let smallest: number = Number.POSITIVE_INFINITY;
@@ -102,11 +112,10 @@ export class WMSolver {
             })
             return smallestValue;
         }
-        console.log(`Solving STAGE ${week}`);
         
-        
-        stage.rows.forEach((row, index) => {
-            const _x = row.previousX;
+        stage.rows.forEach(row => {
+            // Substract the number of employees who just quited the job
+            const _x = row.previousX - this.model.quitEmployees;
             const options: CostOption[] = [];
             
             // Check each value for each row and so compute the minimum of the options
@@ -116,21 +125,16 @@ export class WMSolver {
                     value: calculateCost(b, _x)
                 });
             }
-            console.log(`Options for stage ${week} row ${_x}: `);
-            console.log(options);
-            
             const minCost: CostOption = getMinimum(options);
             row.cost = JSON.stringify(options);
             row.f = minCost.value;
             row.x = minCost.demand;
-            
         });
-        console.log("------------------------------------------------------------------");
-        console.log();
     }
     
-    private findResultPathAndInterpretation() {
+    private findResultPathCostAndInterpretation() {
         const path: number[] = [];
+        const cost: number[] = [];
         const interpretation: string[] = [];
         const weeks = this.model.workforceWeeks.length;
         const getInterpretationAt = (row: StageRow): string => {
@@ -142,20 +146,31 @@ export class WMSolver {
             }
             return `It doesn't change`;
         }
-        let row: StageRow = this.stages[weeks - 1].rows[0];
+        const getF_ = (i: number, demand: number): number => {
+            if(i == 0) return 0; // Last stage
+            const nextStage: Stage = this.stages[i - 1];
+            const lookupRow: StageRow = nextStage.rows.find(row => row.previousX == demand);
+            return lookupRow.f;
+        }
+        // Create this initial row to start iterating next
+        let row: StageRow = {
+            previousX: -1,
+            cost: null,
+            f: -1,
+            x: this.stages[weeks - 1].rows[0].previousX
+        };
         
-        path.push(row.x);
-        interpretation.push(getInterpretationAt(row));
-        
-        // The array is reversed so start from the second stage
-        for(let stageIndex = weeks - 2; stageIndex >= 0; stageIndex--) {
+        // The array is reversed so start from the first stage
+        for(let stageIndex = weeks - 1; stageIndex >= 0; stageIndex--) {
             const stage = this.stages[stageIndex];
             row = stage.rows.find(v => v.previousX == row.x);
             
             path.push(row.x);
+            cost.push(row.f - getF_(stageIndex, row.x));
             interpretation.push(getInterpretationAt(row));
         }
         this.path = path;
+        this.cost = cost;
         this.interpretation = interpretation;
     }
     
@@ -169,6 +184,10 @@ export class WMSolver {
     
     getPath(): number[] {
         return this.path;
+    }
+    
+    getCost(): number[] {
+        return this.cost;
     }
     
     getInterpretation(): string[] {
@@ -214,7 +233,7 @@ export class WMSolver {
         initStages();
         solveStages();
         this.stages.reverse();
-        this.findResultPathAndInterpretation();
+        this.findResultPathCostAndInterpretation();
     }
     
 }
--- src/app/wm/wm.component.ts
+++ src/app/wm/wm.component.ts
@@ -16,16 +16,20 @@ export class WmComponent implements OnInit {
   private newEmployeeFixedCost: number;
   private newEmployeePerWeekCost: number;
   private initialEmployees: number;
+  private fireEmployeeCost: number;
+  private quitEmployees: number;
   private inputData: WorkforcePerWeek[];
   
   constructor() {
     this.solver = new WMSolver();
     this.inputDataStep = 0;
     this.timeWeeks = 5;
-    this.manpowerExcessCost = 200;
-    this.newEmployeeFixedCost = 320;
-    this.newEmployeePerWeekCost = 150;
-    this.initialEmployees = 0;
+    this.manpowerExcessCost = 0;
+    this.newEmployeeFixedCost = 9000;
+    this.newEmployeePerWeekCost = 108000;
+    this.initialEmployees = 30;
+    this.fireEmployeeCost = 25000;
+    this.quitEmployees = 3;
     this.inputData = null;
   }
   
@@ -34,23 +38,23 @@ export class WmComponent implements OnInit {
     const sampleData: WorkforcePerWeek[] = [
       {
         week: 1,
-        workforce: 5
+        workforce: 28
       },
       {
         week: 2,
-        workforce: 9
+        workforce: 30
       },
       {
         week: 3,
-        workforce: 12
+        workforce: 25
       },
       {
         week: 4,
-        workforce: 12
+        workforce: 29
       },
       {
         week: 5,
-        workforce: 6
+        workforce: 20
       }
     ]
     for(let i = 0; i < this.inputData.length; i++) {
@@ -80,6 +84,8 @@ export class WmComponent implements OnInit {
       newEmployeeFixedCost: this.newEmployeeFixedCost,
       newEmployeePerWeekCost: this.newEmployeePerWeekCost,
       initialNumberOfEmployees: this.initialEmployees,
+      fireEmployeeCost: this.fireEmployeeCost,
+      quitEmployees: this.quitEmployees,
       workforceWeeks: this.inputData
     }
     
