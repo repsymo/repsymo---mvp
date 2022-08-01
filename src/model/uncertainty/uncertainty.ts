45b84a8d4e3bbd9413bf6850a2bea413158185e7
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-08-01 00:11:33 -0600

    Add uncertainty models
    These models are a fast prototype.

--- src/model/uncertainty/uncertainty.ts
+++ src/model/uncertainty/uncertainty.ts
@@ -10,3 +10,236 @@
  * tree or at https://opensource.org/licenses/GPL-3.0.
  */
 
+// Super basic decision-making under uncertainty approaches
+
+export interface Uncertainty {
+  payoff: Action[];
+  n?: number;
+  m?: number;
+  isGain?: boolean;
+}
+
+export interface Action {
+  id: number;
+  states: number[];
+}
+
+export interface Minimax {
+  model: Uncertainty;
+  maxima: number[];
+  action: number;
+  minimax: number;
+}
+
+export interface Maximin {
+  model: Uncertainty;
+  minima: number[];
+  action: number;
+  maximin: number;
+}
+
+export interface Maximax {
+  model: Uncertainty;
+  maxima: number[];
+  action: number;
+  maximax: number;
+}
+
+export interface Minimin {
+  model: Uncertainty;
+  minima: number[];
+  action: number;
+  minimin: number;
+}
+
+export function minimax(model: Uncertainty): Minimax {
+  const payoff = model.payoff;
+  const maxima = [];
+
+  for (let a = 0; a < payoff.length; a++) {
+    const max = maxIndex(payoff[a].states);
+    maxima.push(payoff[a].states[max]);
+  }
+  const min = minIndex(maxima);
+  return {
+    model,
+    maxima,
+    action: min,
+    minimax: maxima[min]
+  };
+}
+
+export function maximin(model: Uncertainty): Maximin {
+  const payoff = model.payoff;
+  const minima = [];
+
+  for (let a = 0; a < payoff.length; a++) {
+    const min = minIndex(payoff[a].states);
+    minima.push(payoff[a].states[min]);
+  }
+  const max = maxIndex(minima);
+  return {
+    model,
+    minima,
+    action: max,
+    maximin: minima[max]
+  };
+}
+
+export function maximax(model: Uncertainty): Maximax {
+  const payoff = model.payoff;
+  const maxima = [];
+
+  for (let a = 0; a < payoff.length; a++) {
+    const max = maxIndex(payoff[a].states);
+    maxima.push(payoff[a].states[max]);
+  }
+  const max = maxIndex(maxima);
+  return {
+    model,
+    maxima,
+    action: max,
+    maximax: maxima[max]
+  };
+}
+
+export function minimin(model: Uncertainty): Minimin {
+  const payoff = model.payoff;
+  const minima = [];
+
+  for (let a = 0; a < payoff.length; a++) {
+    const min = minIndex(payoff[a].states);
+    minima.push(payoff[a].states[min]);
+  }
+  const min = maxIndex(minima);
+  return {
+    model,
+    minima,
+    action: min,
+    minimin: minima[min]
+  };
+}
+
+export interface HurwiczUncertainty extends Uncertainty {
+  weight: number;
+}
+
+export interface Hurwicz {
+  model: Uncertainty;
+  weights: number[];
+  action: number;
+  hurwicz: number;
+}
+
+export function hurwicz(model: HurwiczUncertainty) {
+  const { payoff, weight } = model;
+  const weights: number[] = [];
+
+  for (let a = 0; a < payoff.length; a++) {
+    const max = maxIndex(payoff[a].states);
+    const min = minIndex(payoff[a].states);
+    const w = weight * payoff[a].states[max] + (
+      1 - weight
+    ) * payoff[a].states[min];
+    weights.push(w);
+  }
+  const max = maxIndex(weights);
+  return {
+    model,
+    weights,
+    action: max,
+    hurwicz: weights[max]
+  };
+}
+
+export interface Laplace {
+  model: Uncertainty;
+  expectations: number[];
+  action: number;
+  laplace: number;
+}
+
+export function laplace(model: Uncertainty) {
+  const expectations = [];
+
+  for (const a of model.payoff) {
+    expectations.push(uniformExpectation(a.states));
+  }
+  const max = maxIndex(expectations);
+  return {
+    model,
+    expectations,
+    action: max,
+    laplace: expectations[max]
+  };
+}
+
+export interface Savage {
+  model: Uncertainty;
+  regret: number[];
+  action: number;
+  savage: number;
+}
+
+export function savage(model: Uncertainty) {
+  const { payoff, isGain } = model;
+  const maxColumn = idx => maxIndex(payoff.map(row => row[idx]));
+  const minColumn = idx => minIndex(payoff.map(row => row[idx]));
+  const gainValue = (i, j) => payoff[maxColumn(j)][j] - payoff[i][j];
+  const lossValue = (i, j) => payoff[i][j] - payoff[minColumn(j)][j];
+  const regretMatrix = () => {
+    const matrix: Action[] = [];
+
+    for (let i = 0; i < model.n; i++) {
+      matrix.push({ id: i + 1, states: [] });
+      for (let j = 0; j < model.m; j++) {
+        const value = isGain ? gainValue(i, j) : lossValue(i, j);
+        matrix[i].states.push(value);
+      }
+    }
+    return matrix;
+  };
+  const regret = regretMatrix();
+  const mm = minimax({ payoff: regret });
+  return {
+    model,
+    regret,
+    action: mm.action,
+    savage: payoff[mm.action]
+  };
+}
+
+function minIndex(vec: number[]): number {
+  if (vec.length === 0) {
+    return NaN;
+  }
+  let index = 0;
+  let min = vec[index];
+  vec.forEach((value, idx) => {
+    if (value < min) {
+      index = idx;
+      min = value;
+    }
+  });
+  return index;
+}
+
+function maxIndex(vec: number[]): number {
+  if (vec.length === 0) {
+    return NaN;
+  }
+  let index = 0;
+  let max = vec[index];
+  vec.forEach((value, idx) => {
+    if (value > max) {
+      index = idx;
+      max = value;
+    }
+  });
+  return index;
+}
+
+function uniformExpectation(vec: number[]) {
+  const p = 1 / vec.length;
+  return p * vec.reduce((a, b) => a + b, 0);
+}
